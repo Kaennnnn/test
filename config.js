1624b22f2fbbf8c492ed82573f51419b46c8f2af
Kaennnnn <zeitnotdemir@gmail.com>
2025-11-04 08:58:08 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+const _0x3f8e = {
+    _0xa: "AIzaSyDaYJOPgAMLn2vD-khXOGA7oVd22RtzvMw",
+    _0xb: "watchparty-12d55.firebaseapp.com",
+    _0xc: "https://watchparty-12d55-default-rtdb.europe-west1.firebasedatabase.app",
+    _0xd: "watchparty-12d55",
+    _0xe: "watchparty-12d55.firebasestorage.app",
+    _0xf: "551355494947",
+    _0x10: "1:551355494947:web:17205c0efd91b16c547796"
+};
+
+const _0x2c7d = () => {
+    return {
+        apiKey: _0x3f8e._0xa,
+        authDomain: _0x3f8e._0xb,
+        databaseURL: _0x3f8e._0xc,
+        projectId: _0x3f8e._0xd,
+        storageBucket: _0x3f8e._0xe,
+        messagingSenderId: _0x3f8e._0xf,
+        appId: _0x3f8e._0x10
+    };
+};
